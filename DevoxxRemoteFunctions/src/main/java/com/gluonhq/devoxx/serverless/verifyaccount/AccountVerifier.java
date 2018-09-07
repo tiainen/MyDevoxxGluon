351f377045c0ce4feb061aa63f394634e0ea3266
Joeri Sykora <joeri.sykora@gluonhq.com>
2018-09-07 09:58:29 +0200

    Revert "replace http:// with https://"
    This reverts commit 91169d8

--- DevoxxRemoteFunctions/src/main/java/com/gluonhq/devoxx/serverless/verifyaccount/AccountVerifier.java
+++ DevoxxRemoteFunctions/src/main/java/com/gluonhq/devoxx/serverless/verifyaccount/AccountVerifier.java
@@ -49,10 +49,6 @@ public class AccountVerifier {
     private static final Logger LOGGER = Logger.getLogger(AccountVerifier.class.getName());
 
     public String verify(String cfpEndpoint, String email, String password) throws IOException {
-        if (cfpEndpoint.startsWith("http://")) {
-            cfpEndpoint = "https://" + cfpEndpoint.substring("http://".length());
-        }
-
         if (cfpEndpoint.contains("cfp.devoxx.fr")) {
             return verifyDevoxxFr(email, password);
         } else {

ada58b0d324690d3c0610fd62afee81494c1e3bc
Gluon Jenkins <jenkins@gluonhq.com>
2018-10-01 20:48:40 +0200

    new build: 312

--- DevoxxClientCommon/src/main/java/com/devoxx/util/DevoxxSettings.java
+++ DevoxxClientCommon/src/main/java/com/devoxx/util/DevoxxSettings.java
@@ -34,7 +34,7 @@ import java.time.format.FormatStyle;
 import java.util.*;
 
 public class DevoxxSettings {
-    public static final String BUILD_NUMBER = "311"    ;
+    public static final String BUILD_NUMBER = "312"     ;
 
     /**
      * boolean option to switch on/off automatic authentication by using a self generated random UUID

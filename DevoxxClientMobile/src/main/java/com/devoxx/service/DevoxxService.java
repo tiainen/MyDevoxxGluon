7240569d8e1792ba8a052984b8be2f6322f989d3
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2018-10-02 13:05:47 +0530

    #113 Delete previous conference id if its not a number

--- DevoxxClientMobile/src/main/java/com/devoxx/service/DevoxxService.java
+++ DevoxxClientMobile/src/main/java/com/devoxx/service/DevoxxService.java
@@ -1033,4 +1033,14 @@ public class DevoxxService implements Service {
             f.delete();
         }
     }
+
+    private boolean isNumber(String string) {
+        try {
+            Integer.parseInt(string);
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }

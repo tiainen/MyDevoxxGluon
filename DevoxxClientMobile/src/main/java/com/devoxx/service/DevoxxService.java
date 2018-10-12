9ba6e445f1e0d1b316cdc9188d1521ee23c515d2
Joeri Sykora <joeri.sykora@gluonhq.com>
2018-10-12 06:37:02 +0200

    fix error during merge

--- DevoxxClientMobile/src/main/java/com/devoxx/service/DevoxxService.java
+++ DevoxxClientMobile/src/main/java/com/devoxx/service/DevoxxService.java
@@ -1033,14 +1033,4 @@ public class DevoxxService implements Service {
             f.delete();
         }
     }
-
-    private boolean isNumber(String string) {
-        try {
-            Integer.parseInt(string);
-            return true;
-        }
-        catch (NumberFormatException e) {
-            return false;
-        }
-    }
 }
